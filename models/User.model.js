6613a4151734095e349a9d0bfb5e5d562fdcb340
LuanBarreto <luaanr@hotmail.com>
2021-10-07 21:01:32 -0300

    User model criado

---
+++ models/User.model.js
@@ -0,0 +1,20 @@
+const {model, Schema} = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        passwordHash: {
+            type: String,
+            required: true,
+        }
+    },
+    {
+        timestamp: true,
+    }
+);
+
+module.exports = model('User', userSchema);
